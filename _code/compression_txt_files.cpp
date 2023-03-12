fa872951a9b2329a690e7a5336bb2a7cab65c254
Sahil Shah <shahsahils100@gmail.com>
2023-03-13 00:18:14 +0530

    Updated README.md & compression, decompression done

--- _code/compression_txt_files.cpp
+++ _code/compression_txt_files.cpp
@@ -6,31 +6,43 @@ using namespace std;
 
 int main()
 {
+    // Taking the name of file to be compressed
     string s1;
     cout << "Enter filename to be compressed : ";
     cin >> s1;
 
+    // If file not found then give error
     ifstream infile1(s1, ios::in);
     if (!infile1)
     {
         cout << "Infile not found. Press Enter to continue....." << endl;
         cin.ignore();
+        cin.get();
         return 0;
     }
 
+    // Buffer memory & iterator for source data in char form
     vector<char> src(istreambuf_iterator<char>(infile1), {});
 
+    // Buffer memory for compressed data in unsigned char
     vector<unsigned char> dest(src.size() * 2);
     uLongf destlen = dest.size();
 
+    // Using compress2 function from zlib
     compress2(dest.data(), &destlen, reinterpret_cast<const Bytef *>(src.data()), src.size(), Z_BEST_COMPRESSION);
-    dest.resize(destlen);
 
-    cout << "Enter compressed filename : ";
-    cin >> s1;
-    ofstream outfile1(s1, ios::binary | ios::trunc);
+    dest.resize(destlen * 1.5);
+
+    // Opening Output file with extension .bin(binary) in binary form
+    ofstream outfile1("compressed_" + s1 + ".bin", ios::binary | ios::trunc);
+
+    // Writing compressed data to output file in binary form
     outfile1.write(reinterpret_cast<const char *>(dest.data()), dest.size());
+
+    // Can also check for unsuccesful compression through value returned by compress2 function
     cout << "Compression successful. Press Enter to continue.....";
     cin.ignore();
+    cin.get();
+
     return 0;
 }
--- _code/decompression_txt_files.cpp
+++ _code/decompression_txt_files.cpp
@@ -6,32 +6,43 @@ using namespace std;
 
 int main()
 {
+    // Taking the name of file to be decompressed
     string s1;
     cout << "Enter filename to be decompressed : ";
     cin >> s1;
 
+    // If file not found then give error
     ifstream infile1(s1, ios::binary);
     if (!infile1)
     {
         cout << "Infile not found. Press Enter to continue....." << endl;
         cin.ignore();
+        cin.get();
         return 0;
     }
 
+    // Buffer memory & iterator for compressed data to read in unsigned char form
     vector<unsigned char> src(istreambuf_iterator<char>(infile1), {});
 
+    // Buffer memory for uncompressed data in char form
     vector<char> dest(src.size() * 2);
     uLongf destlen = dest.size();
 
+    // Using uncompress function from zlib
     uncompress(reinterpret_cast<Bytef *>(dest.data()), &destlen, src.data(), src.size());
+
     dest.resize(destlen);
 
-    cout << "Enter decompressed filename : ";
-    cin >> s1;
-    ofstream outfile1(s1, ios::trunc);
+    // Opening Output file with original extension of .txt
+    ofstream outfile1("decompressed_" + s1.substr(0, s1.length() - 4), ios::trunc);
+
+    // Writing uncompressed data to output file in readable form
     outfile1.write(reinterpret_cast<const char *>(dest.data()), dest.size());
+
+    // Can also check for unsuccesful decompression through value returned by uncompress function
     cout << "Decompression successful. Press Enter to continue....." << endl;
     cin.ignore();
+    cin.get();
 
     return 0;
 }
